03c67113f8c59f9c51b03758744b8ac97bb470af
Shubhankar Nautiyal <shubhankar.nautiyal21@gmail.com>
2024-09-30 12:46:26 +0530

    Comment out database connection in registration route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 
-    await connectDB();
+    // await connectDB();
 
     const formData = await req.formData();
 
