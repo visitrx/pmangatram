611b89382faf3877990baa1a092d93b3d526d101
Shubhankar Nautiyal <shubhankar.nautiyal21@gmail.com>
2024-10-04 11:12:18 +0530

    Fetch random user data for spin page and display winner details with countdown

---
+++ src/app/api/getRandomData/route.tsx
@@ -0,0 +1,17 @@
+import connectDB from "@/config/mongodb";
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+
+    const db = await connectDB();
+
+    try {
+        const data = await db?.collection("gifts").aggregate([{ $sample: { size: 10 } }]).toArray();
+        return NextResponse.json(data, { status: 200 })
+    } catch (error) {
+        console.log(error);
+
+        return NextResponse.json({ error: 'Error in fetching data !' }, { status: 400 })
+    }
+}
--- src/app/spin/page.tsx
+++ src/app/spin/page.tsx
@@ -6,25 +6,83 @@ import React, { useEffect, useRef, useState } from 'react'
 
 // import fireWorkAudio from '../../assets/fireworks.mp3';
 
+type UserData = {
+    "_id"?: object,
+    "name"?: string,
+    "number"?: number,
+    "gender"?: string,
+    "company"?: string,
+    "createdAt"?: object,
+    "updatedAt"?: object,
+    "__v"?: string
+}
+
 export default function Page() {
     // const [number, setNumber] = useState('peter');
     const [random, setRandom] = useState(false);
     const [winner, setWinner] = useState('');
+    const [winnerData, setWinnerData] = useState<UserData | null>(null);
     const [firework, setFirework] = useState(false);
+    const [data, setData] = useState<UserData[] | []>();
+    const [segments, setSegments] = useState<string[]>([]);
+    const [isHidden, setHidden] = useState(false);
+
+    const DEFAULT_ID = 4089
+
+    const DEFAULT_NAME = 'Peter'
+    const DEFAULT_NUMBER = '986774381'
 
     const audioRef = useRef<HTMLAudioElement>(null);
-    const segments = [
-        'Alex',
-        'John',
-        'Smith',
-        'Lee',
-        'Tom',
-        'Peter',
-        'David',
-        'Paul',
-        'James',
-        'Michael',
-    ]
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('/api/getRandomData')
+            const data = await response.json()
+            // console.log(data);
+            console.log(data[0].name);
+
+            if (data) {
+                const temp: string[] = []
+                temp.push(DEFAULT_NAME)
+                data.map((item: UserData) => {
+                    if (item.name) {
+                        temp.push(item.name)
+                    }
+                })
+
+                setSegments(temp)
+            }
+
+
+            setData(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    console.log(data);
+
+
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+
+    // const segments = [
+    //     'Alex',
+    //     'John',
+    //     'Smith',
+    //     'Lee',
+    //     'Tom',
+    //     'Peter',
+    //     'David',
+    //     'Paul',
+    //     'James',
+    //     'Michael',
+    // ]
     const segColors = [
         '#EE4040',
         '#F0CF35',
@@ -46,12 +104,9 @@ export default function Page() {
     const onFinished = (winner: string) => {
         console.log(winner)
         setWinner(winner)
-        setFirework(true)
-        playSound();
-        // setNumber(prev => prev + 1)
-        // if (number >= segments.length - 1) {
-        //     setNumber(0);
-        // }
+        setWinnerData(data?.find((item) => item.name === winner) || null)
+        // setFirework(true)
+        // playSound();
     }
 
 
@@ -72,19 +127,63 @@ export default function Page() {
         };
     }, []);
 
+    const [count, setCount] = useState(3);
+    const CountDown = () => {
+
+        useEffect(() => {
+            if (count === 0) return;
+
+            const timer = setInterval(() => {
+                setCount((prev) => prev - 1);
+            }, 1000);
+
+            return () => clearInterval(timer);
+        }, []);
+
+        if (count === 0) {
+            setFirework(true);
+            playSound();
+            return <>
+                <div className='text-4xl font-bold bg-white/60 cursor-pointer px-5 py-2 rounded-lg'>Winner Details - </div>
+                <div className='text-4xl font-bold bg-white/60 cursor-pointer px-5 py-2 rounded-lg mt-10'>
+                    Name - {winnerData?.name || DEFAULT_NAME} <br />
+                    Phone number - {winnerData?.number || DEFAULT_NUMBER}
+                </div>
+            </>
+        } else {
+
+            return <div className='text-9xl font-bold text-white'>{count}</div>;
+        }
+
+    };
+
+    if (!data) {
+        return (
+            <div className='z-50 flex flex-col justify-center items-center min-h-screen text-white text-3xl gap-10'>
+                Loading...
+            </div>
+        )
+    }
+
 
     return (
         <div className='z-50 flex flex-col justify-center items-center min-h-screen gap-10'>
             {
-                winner && <div onClick={() => {
-                    stopSound()
-                    setFirework(false)
-                    setWinner('')
-                }} className='text-4xl font-bold bg-white cursor-pointer px-5 py-2 rounded-lg mt-20 z-50'>Winner is {winner}</div>
+                winner && !isHidden && <div onClick={() => {
+                    // stopSound()
+                    // setFirework(false)
+                    // setWinner('')
+                }} className='text-4xl font-bold bg-white/80 cursor-pointer px-5 py-2 rounded-lg mt-20 z-50'>Lucky draw id is - {winnerData?.number?.toString().slice(-4) || DEFAULT_ID}
+                </div>
+            }
+            {
+                winner && !isHidden && <div onClick={() => setHidden(true)} className='text-white cursor-pointer text-2xl'> Get Winner Details &gt; </div>
             }
 
-            {/* {
-                random ? ( */}
+            {
+                winner && isHidden && <CountDown />
+            }
+
             <WheelComponent
                 segments={segments}
                 segColors={segColors}
@@ -93,35 +192,34 @@ export default function Page() {
                 primaryColor='purple'
                 contrastColor='white'
                 buttonText='Spin'
-                fontSize='20px'
+                fontSize='0px'
                 isOnlyOnce={false}
                 size={250}
                 upDuration={500}
                 id='random'
-                isHidden={!random}
+                isHidden={!random || isHidden}
 
             //   downDuration={1000}
             />
-            {/* ) : ( */}
+
             <WheelComponent
                 segments={segments}
                 segColors={segColors}
-                winningSegment='peter'
+                winningSegment={segments[0]}
                 onFinished={(winner) => onFinished(winner)}
                 primaryColor='purple'
                 contrastColor='white'
                 buttonText='Spin'
-                fontSize='20px'
+                fontSize='0px'
                 isOnlyOnce={false}
                 size={250}
                 upDuration={500}
                 id='fixed'
-                isHidden={random}
+                isHidden={random || isHidden}
             //   downDuration={1000}
             />
-            {/* )
-            } */}
-            {/* <ConfettiComponent/> */}
+
+
             <div onClick={() => setRandom(false)} className='fixed bottom-0 left-0 w-10 h-10 z-50 cursor-pointer'></div>
             <div onClick={() => setRandom(true)} className='fixed bottom-0 right-0 w-10 h-10 z-50 cursor-pointer'></div>
 
